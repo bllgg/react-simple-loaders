18cf54835ab40ab1aca54329c8ed9731b8e98a68
Lahiru Gunathilake <gunathilakebllg@gmail.com>
2024-02-11 20:21:37 +0530

    Added Half spinner

---
+++ src/components/spinners/half-spinner/HalfSpinner.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './LoadingAnimation.css';
+
+const HalfSpinner = () => {
+    return (
+        <div className="spinner-container"></div>
+    );
+};
+
+export default HalfSpinner;
