594a69f50e0f0bb538f30d7c60dfc838a440da48
Lahiru Gunathilake <gunathilakebllg@gmail.com>
2024-02-11 19:42:40 +0530

    Added necessary files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import SegmentedBarLoader from "./components/bars/SegmentedBarLoader/SegmentedBarLoader";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+         <SegmentedBarLoader/>
       </header>
     </div>
   );
