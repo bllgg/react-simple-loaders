c6358de9e6b9021777752990b0b218e4f6526180
Lahiru Gunathilake <gunathilakebllg@gmail.com>
2024-02-11 19:44:22 +0530

    Updated the infrastructure files

---
+++ src/components/bars/SegmentedBarLoader/SegmentedBarLoader.tsx
@@ -0,0 +1,19 @@
+// SegmentedBarLoader.tsx
+// @ts-ignore
+import React from 'react';
+import './LoadingAnimation.css'; // Import CSS file
+
+const SegmentedBarLoader: React.FC = () => {
+  return (
+      <div>
+          {/* Your component content */}
+        <div className="loading-animation">
+        <div></div>
+        <div></div>
+        <div></div>
+        </div>
+      </div>
+);
+}
+
+export default SegmentedBarLoader;
